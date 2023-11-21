1c5bf91f1b5adefca029ac8c5ea11932054d6a39
Anushka Singh <anushka@anushkasingh.com>
2023-11-22 01:47:28 +0530

    adding favicon

--- hello.py
+++ hello.py
@@ -1,29 +1,34 @@
-import streamlit
+import streamlit as st
 import github_process
 import gpt4_process
 import pandas as pd
 
-streamlit.header("Kathalyst - Automated Software Documentation")
+st.set_page_config(
+    page_title="Kathalyst",
+    page_icon="images/codeAID_green.png",
+)
 
-github_link = streamlit.sidebar.text_input("Github Link")
-if streamlit.sidebar.button("Submit"):
+st.header("Kathalyst - Automated Software Documentation")
+
+github_link = st.sidebar.text_input("Github Link")
+if st.sidebar.button("Submit"):
     #process if submit button is pressed
     print(f'Processing {github_link}')
     file_contents,file_names,dir = github_process.control(github_link)
 
-    doc,vdd = streamlit.tabs(["Documentation","Visual Dependency Diagram"])
+    doc,vdd = st.tabs(["Documentation","Visual Dependency Diagram"])
     
     with doc:
         # print("\n\nInside Documentation Tab")
         output = gpt4_process.control(file_contents,file_names,dir)
-        streamlit.markdown(output)
+        st.markdown(output)
     with vdd:
         # print("\n\nInside VDD Tab")
-        streamlit.write("Visual Dependency Diagram coming soon ...")
+        st.write("Visual Dependency Diagram coming soon ...")
     
     pass
 
 example_links = ["https://github.com/anushkasingh98/personal-portfolio"]
 df = pd.DataFrame(example_links,columns=["Example Github Links"])
 
-streamlit.sidebar.table(df)
+st.sidebar.table(df)
