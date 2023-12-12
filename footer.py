ac1c26761e4bdc42ca8d5db4ea23ecd012d1c95f
Shimona Dubey <59250522+Shimonad@users.noreply.github.com>
2023-12-11 21:00:32 -0500

    Footer on the sidebar.

--- footer.py
+++ footer.py
@@ -1,15 +1,17 @@
 import streamlit as st
 
 def show_footer():
-    st.markdown(
-        """
-        <div style="position: fixed; bottom: 0; left: 0; width: 100%; text-align: center; padding: 0px;">
-            <p style="color: #f1f1f1; font-family: 'Karla', sans-serif; font-size: 14px; display: inline-block;">For any issues using the product or your account, contact us at</p>
-            <p style="color: #71eea8; font-family: 'Karla', sans-serif; font-size: 14px; display: inline-block;"> hello@kathalyst.ai</p>
-            <p style="color: #f1f1f1; font-family: 'Karla', sans-serif; font-size: 12px; position: inline-block; bottom: 0; right: 0;">© 2023 Kathalyst. All Rights Reserved.</p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+    with st.sidebar:
+        st.markdown(
+            """
+            <div style="position: fixed; bottom: 0; left: 25; text-align: center; padding: 0;">
+                <p style="color: #f1f1f1; font-size: 14px; display: inline-block;">For any issues, contact:</p>
+                <a style="color: #71eea8; font-size: 14px; text-decoration: none; display: inline-block;" href="mailto:hello@kathalyst.ai"> hello@kathalyst.ai</a>
+                <p style="color: #f1f1f1; font-size: 12px; position: inline-block; bottom: 0; right: 0;">© 2023 Kathalyst. All Rights Reserved.</p>
+            </div>
+            """,
+            unsafe_allow_html=True
+            )
+
 
 show_footer()
